eb8097b04b0ddd78ed3914c384895b2b4eb1ad66
endoritos <endysempai@gmail.com>
2023-12-30 16:52:47 +0100

    This show u how to use all reat basic functions like making a buttin and and allet and classes to pass don to it

--- game-libery/src/App.tsx
+++ game-libery/src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+const [alertVisble, setAlertVisbility] = useState(false);
+
   return (
     <div>
-      <Alert><b><span>idk why</span></b> </Alert>
+      { alertVisble && <Alert onClose={()=>setAlertVisbility (false) }>The all</Alert>}
+      <Button  onClick={() => setAlertVisbility(true)}>my button</Button>
     </div>
   );
 }
